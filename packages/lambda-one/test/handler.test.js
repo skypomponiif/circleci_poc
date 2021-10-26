a4f596b2ba17c4bc79fc3ccb4959f9839b62f1ff
Federico Pomponii <federico.pomponii.nttdata@skytv.it>
2021-10-26 15:20:58 +0200

    Added new test

--- packages/lambda-one/test/handler.test.js
+++ packages/lambda-one/test/handler.test.js
@@ -1,14 +1,18 @@
 const mocha = require('mocha');
 const chai = require('chai');
 const should = chai.should();
+const expect = chai.expect;
 
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 const handler = require('../src/handler');
 
 describe("The handler function", () => {
-    it("returns a message", () => {
-        handler.hello(undefined, undefined, function(error, response){
-            let body = JSON.parse(response.body);
-            body.message.should.be.equal('Go Serverless v1.0! Your function executed successfully!');
-        });
+    before(() => {
+        result = handler.hello({}, () => {})
+    })
+
+    it("returns a status code equal 200", () => {
+        return expect(result).to.eventually.have.property('statusCode').equal(200);
     });
 });
